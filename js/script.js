e0a913c64ab8295ff05bd221d2d1d08cf4df3172
Mary Fedirko <fedirko.mary@gmail.com>
2018-11-11 15:21:35 +0200

    cleaned js and images

--- js/script.js
+++ js/script.js
@@ -1,41 +1,6 @@
 $( document ).ready(function() {
 
-    var setClock = 600;
 
-    $('.clock').each(function() {
-        var clock = $(this);
-        clock.FlipClock(setClock, {
-            clockFace: 'HourlyCounter',
-            countdown: true,
-            language: 'it',
-            autoStart:true
-        });
-    });
-
-    $('.faq_list h2').click(function (e) {
-        $(this).next('.result').slideToggle();
-        $(this).toggleClass('active');
-    });
-
-    $('select').niceSelect();
-
-    if (screen.width < 768) {
-        $('.slick').slick({
-            slidesToShow: 2,
-            slidesToScroll: 2,
-            dots: true,
-            arrows:false,
-            responsive: [
-                {
-                    breakpoint: 480,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1
-                    }
-                }
-            ]
-        })
-    }
     function resizeViseo() {
         var video = $('video');
         var videoWidth = video.width();
